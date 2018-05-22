c1cd78d941391b93fcde6c90bb93c18b025ff9b4
Alexander Olmedo <alexander.olmedo@epn.edu.ec>
2018-05-22 16:34:33 -0500

    Conexión MySQL

--- 05-nestjs/src/app.module.ts
+++ 05-nestjs/src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario/usuario.entity";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'web2018agr2.mysql.database.azure.com',
+            port: 3306,
+            username: 'profesor@web2018agr2',
+            password: 'Javascript1',
+            database: 'web',
+            entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+            synchronize: true,
+            ssl: true
+        }),
+        TypeOrmModule.forFeature([UsuarioEntity])
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
